877aa6ed620a9ed7735b5f498344adcdc7106104
Ana-Maria Stratulat <a.m.stratulat@gmail.com>
2025-07-07 00:17:00 +0300

    Add app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Route, HashRouter as Router, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import QuizzesPage from './pages/QuizzesPage';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/quizzes' : '/'}>
+    <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/quiz" element={<QuizzesPage />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 };
 
